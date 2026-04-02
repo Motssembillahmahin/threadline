2da8c0cd39b04546865c9719c3e82210c182a1d5
Motssembillahmahin <motssembillahmahin@gmail.com>
2026-04-03 02:40:48 +0600

    feat: export all models from models package

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .post import Post
+from .comment import Comment
+from .reply import Reply
+from .like import PostLike, CommentLike, ReplyLike
+
+__all__ = ["User", "Post", "Comment", "Reply", "PostLike", "CommentLike", "ReplyLike"]
